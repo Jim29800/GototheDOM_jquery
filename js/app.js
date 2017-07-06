6c7b9d8a2ca68179879a7593e3129d82f0374667
Jim29800 <jbouvier1173@gmail.com>
2017-07-06 14:54:56 +0200

    terminer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log('hello world');  
+$("#addText").html("hello world");
+$("#remove").remove();
+$("#changeColor").addClass("backgroundcolor");
+
+
+for (var i = 0; i < 4; i++) {
+	$("#addChildren").append("<p>yop</p>");
+};
